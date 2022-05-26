70504d4db933597d600275dfe37759a94f233f41
Rob Snow <rsnow@adobe.com>
2022-05-26 14:04:34 -0700

    get closer to our app

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,11 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-links",
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        docs: false,
-      },
-    },
-    "@storybook/addon-interactions",
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-a11y',
     "@storybook/preset-create-react-app"
   ],
   "framework": "@storybook/react",
